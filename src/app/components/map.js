7798ede06aa0bb93942f4fe68a1e803f23b231dd
buryrydz <buryrydz@gmail.com>
2017-12-07 18:43:25 +0100

    23

--- src/app/components/map.js
+++ src/app/components/map.js
@@ -1,6 +1,8 @@
 import React, {Component} from 'react';
 import '../scss/map.scss';
 
+proj4.defs('EPSG:3006', '+proj=utm +zone=33 +ellps=GRS80 +towgs84=0,0,0,0,0,0,0 +units=m +axis=neu +no_defs');
+
 export default class Map extends Component {
     constructor(props) {
         super(props);
@@ -13,9 +15,10 @@ export default class Map extends Component {
     }
 
     initMap() {
+        const projection = ol.proj.get('EPSG:3006');
         const extent = [ -1200000, 4700000, 2600000, 8500000 ];
-        const resolutions = [ 4096.0, 2048.0, 1024.0, 512.0, 256.0, 128.0, 64.0, 32.0, 16.0, 8.0 ];
-        const matrixIds = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ];
+        const resolutions = [ 4096.0, 2048.0, 1024.0, 512.0, 256.0, 128.0, 64.0, 32.0, 16.0, 8.0, 4.0 ];
+        const matrixIds = [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 ];
 
         const tileGrid = new ol.tilegrid.WMTS({
             tileSize : 256,
@@ -71,6 +74,7 @@ export default class Map extends Component {
             logo : false,
             interactions : interactions.extend([dragAndDropInteraction]),
             view : new ol.View({
+                projection: projection,
                 extent : extent,
                 center : [ 616542, 6727536 ],
                 zoom : 1,
