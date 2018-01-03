569712255ddbf3c2220634fb5309362c0e1a7ab9
buryrydz <buryrydz@gmail.com>
2018-01-03 18:07:02 +0100

    16 fix padding for map and menu right

--- src/app/components/map_main.js
+++ src/app/components/map_main.js
@@ -4,11 +4,11 @@ import MenuRight from './menu_right';
 
 export default () => {
     return (
-        <div className="row no-gutters">
-            <div className="col-sm-9">
+        <div className="row">
+            <div className="col-sm-9 p-0">
                 <Map />
             </div>
-            <div className="col-sm-3">
+            <div className="col-sm-3 p-0">
                 <MenuRight />
             </div>
         </div>
